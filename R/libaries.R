3fd9f2704f1cc74799ba0ea497e337e8dc244c64
deboonstra <daniel-boonstra@uiowa.edu>
2024-06-26 17:34:28 -0500

    Updating libraries and suppressing warning messages

--- R/libaries.R
+++ R/libaries.R
@@ -1,2 +1,6 @@
-library(readxl)
-library(dplyr)
+suppressWarnings({
+  library(readxl, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(dplyr, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(stats, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(DescTools, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+})
