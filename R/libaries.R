15b782c89941e65b5d347e75b9fec115fd93e69d
deboonstra <daniel-boonstra@uiowa.edu>
2024-07-01 20:41:49 -0500

    Updating package dependency list

--- R/libaries.R
+++ R/libaries.R
@@ -3,4 +3,7 @@ suppressWarnings({
   library(dplyr, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
   library(stats, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
   library(DescTools, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(knitr, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(kableExtra, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+  library(ggplot2, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
 })
